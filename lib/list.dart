008b92ffdae9fbd41d3800e3b8c9fa3e669eb0f0
yuhueilee <ylee0026@student.monash.edu>
2022-04-20 13:40:37 +0800

    Import Item in List and removed unused methods and variable

--- lib/list.dart
+++ lib/list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:item_list/item.dart';
 
 class List extends StatefulWidget {
   final int itemCount;
@@ -10,8 +11,6 @@ class List extends StatefulWidget {
 }
 
 class _ListState extends State<List> {
-  var _pressedIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return _buildSuggestions(widget.itemCount);
@@ -23,28 +22,7 @@ class _ListState extends State<List> {
         itemBuilder: ((context, index) {
           final Color color =
               Colors.primaries[(index + 1) % Colors.primaries.length];
-          return _buildRow(index, color);
+          return Item(index: index, color: color);
         }));
-  }
-
-  Widget _buildRow(int index, Color color) {
-    final String word = index.toString() + '. 點擊數量' + _pressedIndex.toString();
-
-    return ListTile(
-      title: Text(word),
-      tileColor: color,
-      trailing: ElevatedButton(
-        child: const Text('點擊 + 1'),
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-            primary: Colors.white, onPrimary: Colors.black),
-      ),
-    );
-  }
-
-  void onPressed() {
-    setState(() {
-      _pressedIndex += 1;
-    });
   }
 }
